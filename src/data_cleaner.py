f29eb7784bc7c4eda21d87032b8f946c300f1051
Estuardo Carpio <eccarpio@hotmail.com>
2017-09-28 23:33:00 -0400

    Real data structure learning

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+# LOADS RAW DATA
 with open('../data/q-values.txt', 'r') as f:
     lines = f.readlines()
     recordings = dict()
@@ -11,23 +14,115 @@ with open('../data/q-values.txt', 'r') as f:
             line = line.replace('\n', '').replace('_flip', '100')
             if recordings.get(line, None) is None:
                 count += 1
-                recordings[line] = lines[i + 1]
-            elif recordings[line] != lines[i + 1]:
+                recordings[line] = lines[i + 1].replace('\n', '').replace('\t', ' ')
+            elif recordings[line] != lines[i + 1].replace('\n', '').replace('\t', ' '):
                 count_repeated += 1
-                recordings[line + 'r'] = lines[i + 1]
+                recordings[line + 'r'] = lines[i + 1].replace('\n', '').replace('\t', ' ')
             else:
                 count_discarded += 1
     print 'Data loaded.\nTotal:\t\t{0}\nRepeated:\t{1}\nDiscarded:\t{2}\n'.format(
-        len(recordings), count_repeated, count_discarded)
+        len(recordings.keys()), count_repeated, count_discarded)
 
+# FIRST SWEPT, DEALS WITH FLIPS AND IDENTIFIES BROKEN SAMPLES
 samples = dict()
-
 for key in sorted(recordings.keys()):
     keys = key.split('_')
     if not keys[3].endswith('r'):
-        samples[(keys[0], int(keys[1]), int(keys[2]), keys[3])] = recordings[key]
+        samples[(keys[0], int(keys[1]), int(keys[2]), int(keys[3]))] = recordings[key]
     else:
-        samples[(keys[0], int(keys[1]), -int(keys[2]), keys[3].replace('r',''))] = recordings[key]
+        samples[(keys[0], int(keys[1]), -int(keys[2]), int(keys[3].replace('r', '')))] = recordings[key]
 
+
+# SECOND SWEPT REPAIRS BROKEN SAMPLES, NUMBERS FINAL SAMPLES
+final_samples = dict()
+sample_count = -1
+last_id = 0
 for key in sorted(samples.keys()):
-    print key
+    if key[3] == 0:
+        sample_count += 1
+        final_samples[(key[0], sample_count, key[3])] = samples[key]
+        last_id = key[2]
+    elif key[2] == last_id:
+        final_samples[(key[0], sample_count, key[3])] = samples[key]
+    else:
+        if samples.get((key[0], key[1], -key[2], 0), None) is not None:
+            sample_count += 1
+            final_samples[(key[0], sample_count, 0)] = samples.get((key[0], key[1], -key[2], 0), None)
+            final_samples[(key[0], sample_count, key[3])] = samples[key]
+            last_id = key[2]
+        else:
+            print 'error', key
+
+
+# THIRD SWEPT FIXES INCONSISTENCIES
+for key in final_samples.keys():
+    if key[0] == 'noncompliant':
+        if key[2] == 0:
+            if final_samples.get((key[0], key[1], 1), None) is None:
+                # print final_samples[(key[0], key[1] + 10, 1)]
+                final_samples[(key[0], key[1], 1)] = final_samples[(key[0], key[1] + 10, 1)]
+    else:
+        if key[2] == 0:
+            if final_samples.get((key[0], key[1], 1), None) is None:
+                if final_samples[key].endswith('1'):
+                    # print final_samples[(key[0], 3, 1)]
+                    final_samples[(key[0], key[1], 1)] = final_samples[(key[0], 3, 1)]
+
+# FOURTH SWEPT PREPARES SAMPLES FOR OUTPUT
+curr_samples = dict()
+out_samples = list()
+for key in sorted(final_samples.keys()):
+    if key[0] == 'compliant' and final_samples[key].endswith('2'):
+        if final_samples.get((key[0], key[1], 1), None) is None:
+            curr_sample = dict()
+            curr_sample[('A', 0)] = 1
+            curr_sample[('O', 0)] = 0
+            curr_sample[('R', 0)] = 0
+            curr_sample[('A', 1)] = 0
+            curr_sample[('O', 1)] = 2
+            curr_sample[('R', 1)] = 2
+            out_samples.append(curr_sample)
+        else:
+            curr_sample = dict()
+            curr_sample[('A', 0)] = 1
+            curr_sample[('O', 0)] = 0
+            curr_sample[('R', 0)] = 0
+            curr_sample[('A', 1)] = 1
+            curr_sample[('O', 1)] = 0
+            curr_sample[('R', 1)] = 0
+            out_samples.append(curr_sample)
+            curr_sample = dict()
+            curr_sample[('A', 0)] = 1
+            curr_sample[('O', 0)] = 0
+            curr_sample[('R', 0)] = 0
+            curr_sample[('A', 1)] = 0
+            curr_sample[('O', 1)] = 2
+            curr_sample[('R', 1)] = 2
+            out_samples.append(curr_sample)
+    elif key[0] == 'noncompliant' and final_samples[key].endswith('1'):
+        curr_sample = dict()
+        curr_sample[('A', 0)] = 1
+        curr_sample[('O', 0)] = 0
+        curr_sample[('R', 0)] = 0
+        curr_sample[('A', 1)] = 1
+        curr_sample[('O', 1)] = 0
+        curr_sample[('R', 1)] = 0
+        out_samples.append(curr_sample)
+    elif key[0] == 'noncompliant' and final_samples[key].endswith('3'):
+        curr_sample = dict()
+        curr_sample[('A', 0)] = 1
+        curr_sample[('O', 0)] = 0
+        curr_sample[('R', 0)] = 0
+        curr_sample[('A', 1)] = 1
+        curr_sample[('O', 1)] = 1
+        curr_sample[('R', 1)] = 1
+        out_samples.append(curr_sample)
+
+# LIST FINAL SAMPLES
+# for sample in out_samples:
+#     print sample
+
+# SAVE SAMPLES
+data = pd.DataFrame(out_samples)
+data.to_csv('../data/qval_data.csv')
+
---
+++ src/real_dbn.py
@@ -0,0 +1,68 @@
+from pgmpy.estimators import HillClimbSearchDBN, BicScore
+from pgmpy.inference import DBNInference
+
+import pandas as pd
+import random as rand
+
+data = pd.DataFrame()
+data = data.from_csv('../data/real_data.csv')
+variables = data.columns.values
+if len(variables) > 0:
+    nvars = list()
+    for var in variables:
+        nvar = var.replace('(', '').replace(')', '').replace('\'', '').replace(' ', '').split(',')
+        nvars.append((nvar[0], int(nvar[1])))
+    data.columns = nvars
+
+
+# LEARNS MODEL FROM REAL DATA
+hc = HillClimbSearchDBN(data, scoring_method=BicScore(data))
+print 'Learning model'
+model = hc.estimate(tabu_length=5, max_indegree=2)
+print 'Learning parameters'
+model.fit(data)
+model.initialize_initial_state()
+
+print "Model learned successfully: ", model.check_model()
+
+print model.edges()
+for cpd in model.get_cpds():
+    print cpd
+#
+# dbn_infer = DBNInference(model)
+#
+#
+# def sample(values):
+#     index = 0
+#     rand_val = rand.random()
+#     freq = values[index]
+#     while freq < rand_val:
+#         index += 1
+#         freq += values[index]
+#     return index
+#
+#
+# t = 0
+# robot = 0
+# while True:
+#     print 'Robot (prompt=0, fail=1, reward=2): ', robot
+#     obs = input('Observation (prompt=0, fail=1, reward=2): ')
+#     q = dbn_infer.query(variables=[('A', t)], evidence={('O', t): obs,
+#                                                         ('R', t): robot})
+#     action = sample(q[('A', t)].values)
+#     print 'Action (resp=0, no resp=1): ', action
+#     q = dbn_infer.query(variables=[('R', t + 1)], evidence={('O', t): obs,
+#                                                             ('A', t): action,
+#                                                             ('R', t): robot})
+#     robot = sample(q[('R', t + 1)].values)
+#
+#     if robot != 0:
+#         print 'Robot (prompt=0, fail=1, reward=2): ', robot
+#         print
+#         print
+#         print 'Reset'
+#         robot = 0
+#         t = 0
+#     else:
+#         t += 1
+#     print
