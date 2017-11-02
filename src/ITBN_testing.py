17092f607aecbe0471a7995b973642e8b2ce6f7c
Estuardo Carpio <eccarpio@hotmail.com>
2017-11-02 16:36:59 -0400

    CNN DBN integration with omega_np model

--- src/ITBN_testing.py
+++ src/ITBN_testing.py
@@ -5,9 +5,14 @@ import numpy as np
 model = IntervalTemporalBayesianNetwork()
 model.add_node("Command")
 model.add_node("Wave")
-model.add_node("t_command_wave")
+model.add_edge("Command", 'Wave')
+# model.add_temporal_node("t_command_wave")
 
-raw = np.random.rand(30, 2)
-data = pd.DataFrame(raw, columns=['Command', 'Wave'])
+raw = np.random.randint(0, 4, 30 * 6)
+raw = np.reshape(raw, [-1, 6])
+raw[:, 2] = raw[:, 0] + 1
+raw[:, 3] = raw[:, 1] + 1
+data = pd.DataFrame(raw, columns=['Command_s', 'Wave_s', 'Command_e', 'Wave_e', 'Command', 'Wave'])
 
 model.learn_temporal_relationships(data)
+model.fit(data[['Command', 'Wave']])
