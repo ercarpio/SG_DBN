3108c821a655581ec4ee0dc7ea20c3559777277c
Estuardo Carpio <eccarpio@hotmail.com>
2017-11-08 00:54:13 -0500

    CNN DBN integration with omega_np model

--- src/ITBN_testing.py
+++ src/ITBN_testing.py
@@ -1,22 +1,27 @@
 from pgmpy.models import IntervalTemporalBayesianNetwork
+from pgmpy.estimators import HillClimbSearch, BicScore
 import pandas as pd
 import numpy as np
 
-model = IntervalTemporalBayesianNetwork()
-model.add_node("Command")
-model.add_node("Wave")
-model.add_edge("Command", 'Wave')
+raw = np.random.randint(0, 1, size=(10, 6))
+raw[:, 0] = np.random.randint(0, 4)
+raw[:, 1] = np.random.randint(2, 7)
+raw[:, 2] = raw[:, 0] + np.random.randint(1, 4)
+raw[:, 3] = raw[:, 1] + np.random.randint(1, 4)
+raw[:, 4] = np.random.randint(0, 2)
 
-raw = np.random.randint(0, 4, 30 * 6)
-raw = np.reshape(raw, [-1, 6])
-raw[:, 2] = raw[:, 0] + 1
-raw[:, 3] = raw[:, 1] + 1
-data = pd.DataFrame(raw, columns=['Command_s', 'Wave_s', 'Command_e', 'Wave_e', 'Command', 'Wave'])
+data = pd.DataFrame(raw, columns=['C_s', 'W_s', 'C_e', 'W_e', 'C', 'W'])
+
+hc = HillClimbSearch(data, scoring_method=BicScore(data))
+
+start = IntervalTemporalBayesianNetwork()
+model = hc.estimate(start=start, tabu_length=10, max_indegree=3)
+print model.edges()
 
 model.learn_temporal_relationships(data)
-print model.edges()
 model.add_temporal_nodes()
 print model.edges()
-model.fit(data[['Command', 'Wave', 't_Command_Wave']])
+
+model.fit(data[['C', 'W', 't_C_W']])
 for cpd in model.get_cpds():
     print cpd
