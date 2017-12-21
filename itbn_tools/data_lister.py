7c582254aa6c07a9307c29916d461b66f3dd6d99
Estuardo Carpio <eccarpio@hotmail.com>
2017-12-20 18:17:48 -0600

    change to directory in data lister

--- itbn_tools/data_lister.py
+++ itbn_tools/data_lister.py
@@ -2,7 +2,7 @@ import os
 
 validation_set = list()
 training_set = list()
-for root, dirs, files in os.walk("../ITBN_tfrecords"):
+for root, dirs, files in os.walk("../../../ITBN_tfrecords"):
     for f in files:
         if 'validation' in f:
             validation_set.append(os.path.join(root, f))
